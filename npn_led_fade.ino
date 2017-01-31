07651eb243e8aa36709ee4eae215fefa08db362f
Hunter Allen <allenhm@gmail.com>
2017-01-31 16:08:13 -0500

    Added comments and cleaned-up.

--- npn_led_fade.ino
+++ npn_led_fade.ino
@@ -5,17 +5,21 @@
 const int transistorPin = 9;
 
 void setup() {
-  Serial.begin(9600);
   analogWrite(transistorPin, 0);
+  
+  Serial.begin(9600);
   Serial.println("Beginning LED fade in 5 seconds.");
+  
   delay(5000);
 }
 
 void loop() {
+  // Ramp up to maximum
   for (int x = 0; x <= 255; x++) {
     analogWrite(transistorPin, x);
     delay(50);
   }
+  // Ramp down to minimum
   for (int x = 255; x >= 0; x--) {
     analogWrite(transistorPin, x);
     delay(50);
